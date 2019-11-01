97095041f94a903e25a1b7ac170c2db9db4f482f
YYP-Allen <1519327651@qq.com>
2019-11-02 00:28:50 +0800

    beta

--- js/index.js
+++ js/index.js
@@ -12,56 +12,7 @@ getMusicList(function(list) {
   loadMusic(list[currentIndex])
   setMusicList(list)
 })
-audio.ontimeupdate = function () {
-  console.log(this.currentTime)
-  $('.musicBox .timeNow').style.width = (this.currentTime/this.duration)*100 + '%'
-}
-
-audio.onplay = function () {
-  clock = setInterval(function () {
-    var min = Math.floor(audio.currentTime/60)
-    var sec = Math.floor(audio.currentTime)%60 + ''
-    sec = (sec.length === 2) ? sec : '0' + sec
-    $('.musicBox .playTime').innerText = min + ':' + sec
-  }, 1000)
-}
-audio.onpause = function () {
-  clearInterval(clock)
-}
-audio.onended = function () {
-  currentIndex = (++currentIndex)%musicList.length
-  loadMusic(musicList[currentIndex])
-}
-
-//歌曲的暂停播放
-$('.musicBox .playBtn').onclick = function () {
-  if (audio.paused) {
-    audio.play()
-    this.classList.remove('icon-pause')
-    this.classList.add('icon-play')
-  } else {
-    audio.pause()
-    this.classList.add('icon-pause')
-    this.classList.remove('icon-play')
-  }
-}
-$('.musicBox .btn .icon-step-forward').onclick = function () {
-  currentIndex = (++currentIndex)%musicList.length
-  console.log(currentIndex)
-  loadMusic(musicList[currentIndex])
-}
-$('.musicBox .btn .icon-step-backward').onclick = function () {
-  currentIndex = (musicList.length + (--currentIndex))%musicList.length
-  console.log(currentIndex)
-  loadMusic(musicList[currentIndex])
-}
-
-$('.musicBox .timeProgress .timebar').onclick = function (e) {
-  console.log(e)
-  var percent = e.offsetX / parseInt(getComputedStyle(this).width)
-  console.log(percent)
-  audio.currentTime = audio.duration * percent
-}
+// AJAX获取music资源
 function getMusicList(callback) {
   var xhr = new XMLHttpRequest()
   xhr.open('GET', './music.json', true)
@@ -81,7 +32,71 @@ function getMusicList(callback) {
   xhr.send()
 }
 
-// 获取播放列表
+// 加载歌曲信息
+function loadMusic(musicObj) {
+  // console.log('开始播放', musicObj)
+  $('.musicBox .songName').innerText = musicObj.title
+  $('.musicBox .singer').innerText = musicObj.auther
+  audio.src = musicObj.src
+}
+
+// 播放进度条动态设置
+audio.ontimeupdate = function () {
+  console.log(this.currentTime)
+  $('.musicBox .timeNow').style.width = (this.currentTime/this.duration)*100 + '%'
+}
+// 已播放时间条动态显示
+audio.onplay = function () {
+  clock = setInterval(function () {
+    var min = Math.floor(audio.currentTime/60)
+    var sec = Math.floor(audio.currentTime)%60 + ''
+    sec = (sec.length === 2) ? sec : '0' + sec
+    $('.musicBox .playTime').innerText = min + ':' + sec
+  }, 1000)
+}
+audio.onpause = function () {
+  clearInterval(clock)
+}
+
+// 播放完成，然后自动播放 
+audio.onended = function () {
+  currentIndex = (++currentIndex)%musicList.length
+  loadMusic(musicList[currentIndex])
+}
+
+// 点击歌曲的暂停播放
+$('.musicBox .playBtn').onclick = function () {
+  if (audio.paused) {
+    audio.play()
+    this.classList.remove('icon-pause')
+    this.classList.add('icon-play')
+  } else {
+    audio.pause()
+    this.classList.add('icon-pause')
+    this.classList.remove('icon-play')
+  }
+}
+// 切换到下一曲
+$('.musicBox .btn .icon-step-forward').onclick = function () {
+  currentIndex = (++currentIndex)%musicList.length
+  // console.log(currentIndex)
+  loadMusic(musicList[currentIndex])
+}
+// 切换到上一曲
+$('.musicBox .btn .icon-step-backward').onclick = function () {
+  currentIndex = (musicList.length + (--currentIndex))%musicList.length
+  console.log(currentIndex)
+  loadMusic(musicList[currentIndex])
+}
+// 点击改变进度条
+$('.musicBox .timeProgress .timebar').onclick = function (e) {
+  // console.log(e)
+  var percent = e.offsetX / parseInt(getComputedStyle(this).width)
+  console.log(percent)
+  audio.currentTime = audio.duration * percent
+}
+
+// 获取播放列表信息，展示出来
 function setMusicList(list) {
   var fragment = document.createDocumentFragment()
   var Ul = $('.container .songList')
@@ -105,13 +120,7 @@ $('.container .songList').addEventListener('click', function (e) {
     loadMusic(musicList[currentIndex])
   }
 }, false)
-// 加载歌曲信息
-function loadMusic(musicObj) {
-  console.log('begin play', musicObj)
-  $('.musicBox .songName').innerText = musicObj.title
-  $('.musicBox .singer').innerText = musicObj.auther
-  audio.src = musicObj.src
-}
+
 
 
 
